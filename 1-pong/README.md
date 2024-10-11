08b21b74fff5f6cfd769b40e4bb5633755646912
HenrickyL <HenrickyL1@gmail.com>
2024-10-11 00:58:48 -0300

    pong-1

--- 1-pong/README.md
+++ 1-pong/README.md
@@ -4,8 +4,19 @@
 love .
 ```
 
+## steps
+
+1. The low-res update
+````lua
+love.graphics.setDefaultFilter(min, max) -- point | Bilinear
+love.keypressed(key)
+love.event.quit()
+```
+
+
 ## requirements
-* LOVE2D
+* [x] LOVE2D
 
-
+--------------
 [introduce code](https://www.youtube.com/watch?v=IV60_ZeazSc)
+[base repo](https://github.com/games50/pong)
--- 1-pong/main.lua
+++ 1-pong/main.lua
@@ -1,8 +1,15 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -11,11 +18,19 @@ end
 
 -- default font size is 12 pixels -> use 6 to align
 function love.draw()
-    love.graphics.printf(
-        'Hello Pong',       --text to render
-        0,                  --start X
-        WINDOW_HEIGHT / 2 - 6,  --start Y
-        WINDOW_WIDTH,       --number of pixels to center within
-        'center'            --alignment
-    )
+    -- begin rendering at virtual resolution
+    push:apply('start')
+
+    -- condensed onto one line from last example
+    -- note we are now using virtual width and height now for text placement
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end rendering at virtual resolution
+    push:apply('end')
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
