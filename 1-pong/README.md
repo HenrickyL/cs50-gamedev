c4f00cd12e1eaf13938ef18ac39de58bc592089b
HenrickyL <HenrickyL1@gmail.com>
2024-10-11 01:39:35 -0300

    pong 3 - keyboard e pos

--- 1-pong/README.md
+++ 1-pong/README.md
@@ -19,10 +19,12 @@ love.graphics.newFont(path, size) -- default arial
 love.graphics.setFont(font)
 love.graphics.clear(r,g,b,a)
 love.graphics.rectangle(mode,x,y,width,height)
-
 ```
 
-
+3. The paddle update
+```lua
+love.keyboard.isDown(key)
+```
 ## requirements
 * [x] LOVE2D
 
--- 1-pong/main.lua
+++ 1-pong/main.lua
@@ -6,11 +6,15 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     -- more "retro-looking" font object we can use for any text
     smallFont = love.graphics.newFont('font.ttf', 8)
+
+    scoreFont = love.graphics.newFont('font.ttf', 32)
 
     -- set LÖVE2D's active font to the smallFont obect
     love.graphics.setFont(smallFont)
@@ -20,7 +24,19 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    -- VARIABLES
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+
+    lastDt = 0
+
 end
+
+
 
 -- default font size is 12 pixels -> use 6 to align
 function love.draw()
@@ -31,13 +47,25 @@ function love.draw()
 
     -- condensed onto one line from last example
     -- note we are now using virtual width and height now for text placement
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.setFont(smallFont)
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
-    -- render first paddle (left side)
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    --drawFPS
+    local fps = string.format("%.0f",1/lastDt)
+    love.graphics.setFont(smallFont)
+    love.graphics.printf(fps, 0, 5, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, 
+        VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
+        VIRTUAL_HEIGHT / 3)
+
+    -- render first paddle (left side), now using the players' Y variable
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     -- render second paddle (right side)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
     -- render ball (center)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
@@ -50,4 +78,29 @@ function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
+end
+
+
+function love.update(dt)
+
+    lastDt = (lastDt + dt)/2
+
+    --player 1
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    -- player 2
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+    -- local x = 10
+    -- for i = 0, 40000000 do
+    --     x = x + 1
+    -- end
+    
 end
